b80b09d464ec875f4eebb54d516abce5e51f313b
nergia <matheussr000@gmail.com>
2023-12-01 16:21:10 -0300

    refactor: alter web.php

--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -19,6 +19,4 @@ Route::get('/', [EventController::class,'index']);
 Route::get('/events/create', [EventController::class,'create']); 
 Route::get('/events/{id}', [EventController::class,'show']); 
 
-//Olá
-route::post('/events');
 Route::post('/events', [EventController::class, 'store']);
