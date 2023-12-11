1bd7b0981c7e83b6b1b639b0e1ad33495283adaa
nergia <matheussr000@gmail.com>
2023-12-11 16:32:05 -0300

    Update / edit

--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -21,7 +21,10 @@ Route::get('/events/{id}', [EventController::class,'show']);
 
 Route::post('/events', [EventController::class, 'store']);
 
-Route::delete('/events/{id}',[EventController::class, 'destroy']);
+Route::get('/events/edit/{id}', [EventController::class, 'edit'])->middleware('auth');
+Route::put('/events/update/{id}',[EventController::class, 'update'])->middleware('auth');
+
+Route::delete('/events/{id}',[EventController::class, 'destroy'])->middleware('auth');
 
 
 
